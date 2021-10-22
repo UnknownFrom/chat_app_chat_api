9f8528e1bf0b14110a8f0f611b05cd8ddd713f74
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-10-22 16:35:24 +0300

    Перемещение файлов в chat-api

--- server.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "./composer/vendor/autoload.php";
-$client = new \phpcent\Client("http://localhost:8086");
-$client->setSecret("qwtwetwqgbert");
-
-if(isset($_POST['publish']) && trim($_POST['publish']) !== '')
-{
-    echo 'ok';
-}
