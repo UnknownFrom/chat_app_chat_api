148731ccbc3213b219fc48f113f9ba86fe806b30
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-11-04 15:44:13 +0300

    Изменение

--- server.js
+++ server.js
@@ -191,9 +191,9 @@ function connectToBD() {
     connection = mysql.createConnection({
         host: process.env.MYSQL_HOST,
         port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_ROOT_USER,
+        user: process.env.MYSQL_USER,
         database: process.env.MYSQL_DATABASE,
-        password: process.env.MYSQL_ROOT_PASSWORD
+        password: process.env.MYSQL_PASSWORD
     });
 }
 
